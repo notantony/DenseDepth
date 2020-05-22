295f0918d0975deafb51495d0f7018e2b840ea82
Anton Chekashev <not.antony@yandex.ru>
2020-05-22 21:49:06 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+if [ ! -e "./kitti.h5" ]; then
+  wget -P "./kitti.h5" "https://s3-eu-west-1.amazonaws.com/densedepth/kitti.h5"
+fi
+
 python main.py \
     --host "0.0.0.0" \
     --port 5052
